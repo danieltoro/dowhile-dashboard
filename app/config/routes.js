3c9ddd6c8c4211d0ebd92b6c732921c9f34d2766
Daniel Toro <d.toro@outlook.com>
2017-04-27 20:18:06 -0300

    primer commit

---
+++ app/config/routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Main from '../components/layouts/Main';
+import Blank from '../components/layouts/Blank';
+
+import MainView from '../views/Main';
+import MinorView from '../views/Minor';
+import CategoriasView from '../views/categorias';
+import ProductosView from '../views/productos';
+
+import { Route, Router, IndexRedirect, browserHistory} from 'react-router';
+
+export default (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRedirect to="/main" />
+            <Route path="main" component={MainView}> </Route>
+            <Route path="categorias" component={CategoriasView}> </Route>
+            <Route path="productos" component={ProductosView}> </Route>
+            <Route path="minor" component={MinorView}> </Route>
+        </Route>
+    </Router>
+
+);
